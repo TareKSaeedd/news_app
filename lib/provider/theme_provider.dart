7959c35ae7cf6cf203283131651190e1b940ec86
Tarek <Tareksaeed11111@gmail.com>
2025-08-08 14:55:04 +0300

    Add custom splash screen and branding images for Android and iOS
    - Introduced new drawable resources for Android 12 splash screen and branding.
    - Updated launch_background.xml to include new images for splash screen.
    - Created styles for dark mode and light mode in values-night and values-v31.
    - Added new images for splash branding in both light and dark modes.

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  var themeMode = ThemeMode.light;
+  var themeMode = ThemeMode.dark;
   String themeModeText = 'Light';
 
   void changeAppTheme(BuildContext context) {
