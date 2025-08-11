ffa688397fb58c2e7c2586042cc2ae058c8c7643
Tarek <Tareksaeed11111@gmail.com>
2025-08-11 15:52:25 +0300

    - add images to assets folder with adding it's constants in app assets file

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -2,4 +2,20 @@ class AppAssets {
   static const iconHome = 'assets/images/icon_home.png';
   static const iconTheme = 'assets/images/icon_theme.png';
   static const iconLanguage = 'assets/images/icon_language.png';
+
+  static const generalLightImage = 'assets/images/general_light_image.png';
+  static const businesLightImage = 'assets/images/bussines_light_image.png';
+  static const sportsLightImage = 'assets/images/sports_light_image.png';
+  static const entertainmentLightImage = 'assets/images/entertainment_light_image.png';
+  static const healthLightImage = 'assets/images/health_light_image.png';
+  static const technologyLightImage = 'assets/images/technology_light_image.png';
+  static const scienceLightImage = 'assets/images/science_light_image.png';
+
+  static const generalDarkImage = 'assets/images/general_dark_image.png';
+  static const businessDarkImage = 'assets/images/business_dark_image.png';
+  static const sportsDarkImage = 'assets/images/sports_dark_image.png';
+  static const entertainmentDarkImage = 'assets/images/entertainment_dark_image.png';
+  static const healthDarkImage = 'assets/images/health_dark_image.png';
+  static const technologyDarkImage = 'assets/images/technology_dark_image.png';
+  static const scienceDarkImage = 'assets/images/science_dark_image.png';
 }
